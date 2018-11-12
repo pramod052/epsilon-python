9dbb4a47f6f153502445c8940ecb07b9064f1e9c
root <root@ip-172-31-27-173.ap-southeast-1.compute.internal>
2018-11-12 09:32:32 +0000

    Adding strings.py

---
+++ pythonscripts/strings.py
@@ -0,0 +1,16 @@
+a = "zekelabs"
+print (a[::-1])
+
+
+z = a.replace('zeke','ZEKE')
+print(z.count("E"))
+print(z)
+
+print(z.center(25,"*"))
+print(z.rjust(25,"#"))
+
+
+## get inputs
+#s = raw_inputs()
+s = input("Please enter a string: ")
+print(s.rjust(25,"#"))
